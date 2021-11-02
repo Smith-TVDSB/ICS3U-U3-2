2e833848f46f45e490909c8ae9064198a9441113
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-11-02 14:55:34 -0400

    debugged additional func

--- additionalFunctions.hpp
+++ additionalFunctions.hpp
@@ -5,7 +5,7 @@ using namespace std;
 bool inArray(int arr[25], int key)
 {
     for(int k = 0; k < 25; k++){
-        if(arr[i] == key){
+        if(arr[k] == key){
             return true;
         }
     }
