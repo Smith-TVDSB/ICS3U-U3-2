0e785682cb5871532f358011580bb3ec7c092d06
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2021-11-02 14:51:18 -0400

    debugged test

--- tests.cpp
+++ tests.cpp
@@ -9,17 +9,7 @@
 
 //TESTS
 
-int primes[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97}
-
-bool inArray(int arr[], int key)
-{
-    for(int k = 0; k < 25; k++){
-     if(arr[i] == key){
-         return true;
-     }
-}
-return false;
-}
+int primes[25] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
 TEST_CASE("Exercise One") //Named ExerciseOne, tagged with example
 {
@@ -49,9 +39,18 @@ TEST_CASE("Exercise Three")
     CHECK(exerciseThree(1)==false);
     CHECK(exerciseThree(2)==true);
     CHECK(exerciseThree(13)==true);
+    bool inArray(int arr[25], int key)
+    {
+        for(int k = 0; k < 25; k++){
+            if(arr[i] == key){
+                return true;
+            }
+        }
+        return false;
+    }
     for (int j=2; j<=100;j++)
     {
-        if(inArray(primes,j) )
+        if(inArray(primes,j))
         {
             REQUIRE(exerciseThree(j)==true);
         }
